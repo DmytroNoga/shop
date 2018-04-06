7cde0d09df9aa5fc26862ac1fa70829bea44f23d
DmytroNg <dmytro.noga@gmail.com>
2018-04-06 20:10:20 +0300

    revert last

--- shop/index.php
+++ shop/index.php
@@ -6,10 +6,13 @@ include 'head.php';
 
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 echo "Good bye!";
 
 echo "Hello world!!!";
 
+=======
+>>>>>>> parent of 54eae4d... Added good bye
 =======
 >>>>>>> parent of 54eae4d... Added good bye
 ?>
